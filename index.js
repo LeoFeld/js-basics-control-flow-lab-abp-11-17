c2324d6091b2eb071d6f7aa1aa35bbdb1fac4580
Leo Feld <leofredrickfeld@gmail.com>
2017-11-13 19:31:51 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -12,3 +12,14 @@ function scuberGreetingForFeet (ride){
 function ternaryCheckCity (city){
   return (city === "NYC") ? "Ok, sounds good." : "No go."
 }
+
+function switchOnCharmFromTip(tipString){
+  switch(tipString){
+    case "generous"
+     return "Thank you so much."
+    case "not as generous"
+     return "Thank you."
+    case "thanks for everything"
+     return "Bye."
+   }
+}
